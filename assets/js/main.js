465b7da66012024960f2a1ba860e368c6752d0a2
JonathanSaliba <Jonathan.saliba@ontariotechu.net>
2022-06-18 09:50:08 -0400

    Completed Contact us and started  gallaryy

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,33 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
         }
     }
   });
-/*=============== VALUE ACCORDION ===============*/
+/*=============== Gallary Image Filter ===============*/
+$(document).ready(function () {
 
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
@@ -110,3 +135,5 @@ const sr = ScrollReveal({
 sr.reveal('.home__title, .service__container, .footer__container')
 sr.reveal('.home__description, footer__info', {delay: 500})
 sr.reveal('.home__images', {delay: 800, origin: 'bottom'})
+sr.reveal('.contactus__content', {origin: 'left'})
+sr.reveal('.contactus__images', {origin: 'right'})
